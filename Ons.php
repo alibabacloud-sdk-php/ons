e9233e0a9910b706dfba6c66d775065d75302acc
宋神宗 <i@songshenzong.com>
2019-04-19 09:36:41 +0800

    Supported Sas, Ivision. (#104)

--- Ons.php
+++ Ons.php
@@ -8,6 +8,7 @@ use AlibabaCloud\VersionResolverTrait;
  * Resolve version based on the method name.
  *
  * @method V20170918\OnsApiResolver v20170918()
+ * @method V20190214\OnsApiResolver v20190214()
  */
 class Ons
 {
